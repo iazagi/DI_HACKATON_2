9d84153b8603b6156bd62aaa0c71f37bd868f625
iazagi <101048623+iazagi@users.noreply.github.com>
2022-05-09 23:59:49 +0300

    starting conction to db

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
+const dotenv =require('dotenv');
+//const knex = require('knex');
+//const {} = require('./modules/journal_db.js');
+const DB = require('./modules/journal_db.js');
+dotenv.config();
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,31 +17,55 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 const posts = [];
 app.set('view engine', 'ejs');
-
+dotenv.config();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 /// ROUTS
 app.get("/", (req,res)=>{
-  res.render('home',{
-    startContent:homeStartingContent,
-    posts: posts
-  });
+ res.render('home',{
+   startContent:homeStartingContent,
+   posts: posts
+ });
 });
+app.get('/api/journal_db',(req,res)=>{
+  getAllJournal()
+  .then(
+    res.render('home',{
+        startContent:homeStartingContent,
+       posts: posts
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+  )
+});
+
+
 app.get("/about", (req,res)=>{
   res.render('about',{aboutContent:aboutContent});
 });
 
 app.get("/contact", (req,res)=>{
   res.render('contact',{contactContent:contactContent});
-})
+});
+
 app.get("/compose", (req,res)=>{
   res.render('compose');
 });
+app.post('/api/journal_db',(req,res)=>{
+  addToDataBase(req.body)
+  .then(data=>{
+    res.json(data)
+  })
+  .catch(err =>{
+    console.log(err);
+  })
+})
 app.post('/compose', (req,res)=>{
   //console.log(req.body.postTitile);
   let post = {
-    title: req.body.postTitile,
+    title: req.body.postTitle,
     content: req.body.postBody
   }
   posts.push(post);
@@ -43,15 +73,28 @@ app.post('/compose', (req,res)=>{
   //console.log(post);
 });
 
-app.get('/posts/:postName', (req, res)=>{
-  console.log(req.params.postName);
+app.get('/post/:postTitle', (req, res)=>{
+  const request = _.lowerCase(req.params.postTitle);
+  res.render('post');
+  posts.forEach((post) => {
+    const storedTitle =  _.lowerCase(post.title);
+    if(storedTitle === request){
+      res.render('post', {
+         title : post.title,
+         content : post.content
+      });
+
+
+    };
+  });
+
 });
 
 
+console.log(DB.getAllJournal);
 
 
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, ()=> {
+  console.log(`Server started on 3000 `);
 });
